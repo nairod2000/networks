bae2f27f5fce0e74d2483f3646ea7d403f893a28
nairod2000 <dojmorrissey@gmail.com>
2020-08-15 09:12:34 -0500

    perceptron is a file messing around with some of the necessary ideas for a neural network. network will be my implementation for the mnist dataset

---
+++ network.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# need to look up important abstractions for a network to have. 
+# ex: should layers be a flexible thing, should the input layer count as a layer, how to represent
+# amount of neurons in each layer etc.
+
+class Network:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
