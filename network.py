fixed forward and got backprop to run

network.py
@@ -8,7 +8,7 @@ class Network:
     def __init__(self, sizes):
         self.layers = len(sizes)
         self.sizes = sizes
-        self.weights = [np.random.randint(-2, 3, (i, j)) for j, i in zip(sizes[:-1], sizes[1:])]
+        self.weights = [np.random.randint(-2, 3, (j, i)) for j, i in zip(sizes[:-1], sizes[1:])]
         self.biases = [np.random.randint(-2, 3, (i, 1)) for i in sizes[1:]]
 
     def forward(self, X):
@@ -23,22 +23,26 @@ class Network:
         - A list of each activation layer
         '''
         activations = list()
-        axons = np.matmul(self.weights[0], X) + self.biases[0]
-        activation = self.sigmoid(axons)
-        activations.append(activation)
+        activations.append(X)
+        unactivated_layer = np.matmul(self.weights[0].T, X) + self.biases[0]
+        activated_layer = self.sigmoid(unactivated_layer)
+        activations.append(activated_layer)
         for i in range(1, self.layers-1):
-            axons = np.matmul(self.weights[i], activation) + self.biases[i]
-            activation = self.sigmoid(axons)
-            activations.append(activation)
+            unactivated_layer = np.matmul(self.weights[i].T, activated_layer) + self.biases[i]
+            activated_layer = self.sigmoid(unactivated_layer)
+            activations.append(activated_layer)
         return activations
 
     def back_prop(self, neurons, expected):
         '''
         inputs:
+
         neurons:
         - a list of the all the activated neuron layers
         expected:
         - what the network was supposed to output
+        X:
+        - what the input for the sample was
         --------------------------------------
         return:
         - the gradients of all the layers weights
@@ -50,20 +54,17 @@ class Network:
         n = 0
         while n < len(self.weights):
             current_gradient = cost_to_L
-            activation_to_z1 = self.sigmoid_prime(np.matmul(self.weights[-1], neurons[-2]))
+            activation_to_z1 = self.sigmoid_prime(np.matmul(self.weights[-1].T, neurons[-2]))
             current_gradient *= activation_to_z1
             if n != 0:
                 for i in range(n):
                     z_to_activation = self.weights[-1 - i]
-                    activation_to_z = self.sigmoid_prime(np.matmul(self.weights[-2 - i], neurons[-3 - i]))
-                    current_gradient *= (z_to_activation * activation_to_z)
-            print(current_gradient.shape)
-            current_gradient *= neurons[-2 - n]
-            quit()
+                    current_gradient = np.matmul(z_to_activation, current_gradient)
+                    activation_to_z = self.sigmoid_prime(np.matmul(self.weights[-2 - i].T, neurons[-3 - i]))
+                    current_gradient *= activation_to_z
+            current_gradient = np.matmul(current_gradient, neurons[-2 - n].T)
             n += 1
             gradients.append(current_gradient)
-            if n == len(self.weights):
-                break
         return gradients
 
     @staticmethod
@@ -89,5 +90,15 @@ if __name__ == '__main__':
     network = Network([10, 5, 4, 3])
     X = np.random.randint(0, 4, (10, 1))
     activations = network.forward(X)
-    [print(x.shape) for x in activations]
-    #network.back_prop(activations, [1, 0, 0])
+    print(len(activations))
+    #print('neurons')
+    #[print(x.shape) for x in activations]
+    #print('--------')
+    print('weights')
+    [print(x.shape) for x in network.weights]
+    #print('--------', '\n', 'backprop notes')
+    deltas = network.back_prop(activations, [1, 0, 0])
+    print('-------')
+    print('deltas')
+    [print(x.shape) for x in deltas]
+    #print(len(deltas))