d72b77fc524007140de88b890f6e99c3c739dd5f
nairod2000 <dojmorrissey@gmail.com>
2020-09-26 15:10:28 -0500

    added apply gradients. Model is not learning

--- convo_network.py
+++ convo_network.py
@@ -41,12 +41,6 @@ class Layer:
     def add_to_gradients(cls):
         pass
 
-    @classmethod
-    def clear_history(cls):
-        cls.differentiable_operations = 0
-        cls.operation_history = dict()
-        cls.gradients = dict()
-
     @staticmethod
     def is_row_vector(vector):
         pass
@@ -105,6 +99,8 @@ class Layer:
             Layer.gradients[self.operation_number]['weights'] = weight_grad
 
         def _linear_derivative_bias(self, grad):
+            grad = np.sum(grad, axis=1)
+            grad = grad[:, np.newaxis]
             Layer.gradients[self.operation_number]['bias'] = grad
 
         def _forward(self, X):
@@ -128,7 +124,7 @@ class Layer:
             return result
 
         def derivative(self, X):
-            return self._sigmoid(X) / (1 - self._sigmoid(X))
+            return self._sigmoid(X) * (1 - self._sigmoid(X))
 
         @staticmethod
         def _sigmoid(X):
@@ -147,16 +143,15 @@ class Optimizer(Layer):
         self.operations = Layer.operation_history
         self.gradients = Layer.gradients
 
-    def get_gradients(self, expected):
+    def get_gradients(self, expected):#
         num_operations = len(self.operations)
         
+        # derivative from cost to activation
         last_layer_result = self.operations[num_operations]['result']
-
         cost_to_activation = self.cost_derivative_SGD(last_layer_result, expected.T)
 
         current_gradient = cost_to_activation
         for oper_idx in range(num_operations, 0, -2):
-
             # get necessary objects and relavent data
             non_lin_operation = self.operations[oper_idx]
             non_lin_object = non_lin_operation['object']
@@ -174,21 +169,47 @@ class Optimizer(Layer):
 
             if oper_idx != 2:
                 # derivative from linear eq to prev activation
-                #prev_weight = self.operations[oper_idx-3]['object'].weight
                 prev_weight = lin_object.weight
                 
                 # update gradient
                 current_gradient = np.matmul(prev_weight, current_gradient)
 
 
-    def apply_gradients(self):
-        # will use the self.gradiets to update the gradients of linear objects 
-        # in Linear
-        pass
+    def apply_gradients(self, lr):
+        ''' 
+        This is going to have 
+        '''
+        gradients = Layer.gradients
+        operations = Layer.operation_history
+        num_operations = len(Layer.operation_history) - 1
+        for op in range(num_operations, 0, -2):
+            # Get current objects and relavant data
+            lin_object = operations[op]['object']
+            weight_delta = gradients[op]['weights']
+            bias_delta = gradients[op]['bias']
+
+            # apply deltas
+            lin_object.weight -= (lr * weight_delta)
+            lin_object.bias -= (lr * bias_delta)
 
     @staticmethod
     def cost_derivative_SGD(X, y):
-        return (X - y)
+        try:
+            return (X - y)
+        except ValueError:
+            X_examples = X.shape[1]
+            y_examples = y.shape[1]
+            missing = X_examples - y_examples
+            dim = X.shape[0]
+            blank_ex = np.zeros((dim, missing))
+            y = np.append(y, blank_ex, axis=1)
+            return (X - y)
+
+    @staticmethod
+    def clear_history():
+        Layer.differentiable_operations = 0
+        Layer.operation_history = dict()
+        Layer.gradients = dict()
 
 
 class Network(Layer):
@@ -212,15 +233,37 @@ class Network(Layer):
 
 if __name__ == '__main__':
     loader = MNISTLoader(1)
-    X_data, y_data = loader.load_train()
+    epochs = 1
+    X_train, y_train = loader.load_train()
+    X_test, y_test = loader.load_test()
+
 
     model = Network()
     optimizer = Optimizer()
-    for ex in range(1):
+    for _ in range(epochs):
+        for ex in range(len(X_train)):
 
-        model.forward(X_data[ex].T)
+            model.forward(X_train[ex].T)
 
-        optimizer.get_gradients(y_data[ex])
+            optimizer.get_gradients(y_train[ex])
 
-        ops = Layer.operation_history
-        grads = Layer.gradients
+            optimizer.apply_gradients(.1)
+            optimizer.clear_history()
+
+    correct = 0
+    total = 0
+    for ex in range(len(X_test)): # len(X_test)
+        model.forward(X_test[ex].T)
+        real = y_test[ex]
+        pred = Layer.operation_history[4]['result'].T
+
+        real = real.argmax(axis=1)
+        pred = pred.argmax(axis=1)
+        for i in range(len(real)):
+            total += 1
+            if real[i] == pred[i]:
+                correct += 1
+
+        optimizer.clear_history()
+
+    print(correct/ total)
